7a0141dd6b5a4a04d2cd3d773616d6fd07bd356c
Renat Sedlovsky <ren.sedlovskij2009@yandex.ru>
2024-10-03 12:14:31 +0500

    Решение задач модуля 3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,19 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+  const props = path.split(".");
+  return function (obj) {
+    let search_obj = obj;
+    for (const prop of props) {
+      if (typeof search_obj[prop] == "object" && search_obj[prop] != null) {
+        search_obj = search_obj[prop];
+      } else if (!search_obj.hasOwnProperty(prop)) {
+        return undefined;
+      } else {
+        return search_obj[prop];
+      }
+    }
+  };
 }
