a852d73cabc8dff5293411bb684462bd3a216a60
Renat Sedlovsky <ren.sedlovskij2009@yandex.ru>
2024-10-16 23:07:40 +0500

    Решение задачи 4 модуля

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,73 @@
 export default class ColumnChart {
+  element;
+  chartHeight = 50;
 
+  constructor(props = {}) {
+    const {
+      data = [],
+      label = "",
+      value = 0,
+      link = "",
+      formatHeading = (value) => value,
+    } = props;
+    this.data = data;
+    this.label = label;
+    this.value = formatHeading(value);
+    this.link = link;
+
+    this.element = this.createElement();
+  }
+  createColumns() {
+    const maxValue = Math.max(...this.data);
+    return this.data
+      .map((value) => {
+        const percent = ((value / maxValue) * 100).toFixed(0);
+        return `<div style="--value: ${Math.floor(
+          (value * this.chartHeight) / maxValue
+        )}" data-tooltip="${percent}%"></div>`;
+      })
+      .join("");
+  }
+
+  createLink() {
+    return `${
+      this.link
+        ? `<a class="column-chart__link" href=${this.link}>View all</a>`
+        : ""
+    }`;
+  }
+
+  createTemplate() {
+    return `
+    <div class="column-chart" style="--chart-height: 50">
+      <div class="column-chart__title">
+        ${this.label}
+        ${this.createLink()}
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">${
+          this.value
+        }</div>
+        <div data-element="body" class="column-chart__chart">
+          ${this.createColumns()}
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  createElement() {
+    const element = document.createElement("div");
+    element.innerHTML = this.createTemplate();
+    element.classList.add("column-chart_loading");
+    return element;
+  }
+
+  destroy() {}
+
+  update() {}
+
+  remove() {
+    this.element.remove();
+  }
 }
